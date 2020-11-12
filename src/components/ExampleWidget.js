1473acf706d4f1c1266a3c9c511c76448838402b
Magnus Appel <68828420+magnuss86@users.noreply.github.com>
2020-11-12 13:23:59 -0800

    attack widget test

--- src/components/ExampleWidget.js
+++ src/components/ExampleWidget.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Box, Button, Heading, Text } from 'rebass';
 import Widget from './Widget';
+import { Label, Input } from '@rebass/forms'
 
 // Usage: <ExampleWidget>Hello World!</ExampleWidget>
-export default class ExampleWidget extends Widget {
+export default class AttacksWidget extends Widget {
 
     render() {
         return (
             <Box {...this.props} className={"widget example-widget"}>
-                <Heading>Example Widget</Heading>
+                <Heading>Attack Widget</Heading>
                 <Button>Beep</Button>
+                <Label htmlFor='Attack Widget'>Attack Name</Label>
+                <Input
+                    id='attackName'
+                    name='Attack'
+                    type='text'
+                    placeholder='Enter Attack Name '
+                />
                 <Text>{this.props.children}</Text>
             </Box>
         )
