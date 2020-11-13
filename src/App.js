4c971175317ba503722c9c9f3deb674d6c1b9c8e
Evan K <EvanK311@gmail.com>
2020-11-13 11:52:48 -0800

    full outline of inventory widget

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import BasicWidget from './components/BasicWidget';
 import AttackWidget from './components/AttacksWidget';
+import InventoryWidget from './components/InventoryWidget';
 import Widget from './components/Widget';
 import renderWidgets from './WidgetRenderer';
 import DoubleCheckbox from './components/DoubleCheckbox'
@@ -9,11 +10,11 @@ import theme from '@rebass/preset'
 import { Tiles } from '@rebass/layout'
 
 let widgets = [
-  new AttackWidget({x: 7, children: "Hello"}),
-  new AttackWidget({x: 6, children: "my"}),
-  new AttackWidget({x: 2, children: "baby!"}),
-  new AttackWidget({x: 4, children: "Hello"}),
-  new AttackWidget({x: 5, children: "my"}),
+  // new AttackWidget({x: 7, children: "Hello"}),
+  // new AttackWidget({x: 6, children: "my"}),
+  // new AttackWidget({x: 2, children: "baby!"}),
+  // new AttackWidget({x: 4, children: "Hello"}),
+  new InventoryWidget({x: 5, children: "my"}),
   new AttackWidget({x: 3, children: "honey!"})
 ]
 
@@ -21,7 +22,7 @@ function App() {
   return (
     <ThemeProvider className="App" theme={theme}>
       <Tiles columns={[1, 2, 4]}>
-        <ExampleWidget></ExampleWidget>
+       
         
         {renderWidgets(widgets)}
         <DoubleCheckbox />
