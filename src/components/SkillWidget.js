dcd0cc7d9a184504d0743c8b14e3635e6d47154b
Quint Turner <henryquintturner@gmail.com>
2020-11-13 11:11:34 -0800

    finished rough draft

--- src/components/SkillWidget.js
+++ src/components/SkillWidget.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import Widget from './Widget';
 import { ThemeProvider } from 'emotion-theming'
-import { Box, Card, Image, Heading, Text, Button } from 'rebass'
-import theme from '@rebass/preset'
-import { Tiles } from '@rebass/layout'
+import { Box, Heading, Text } from 'rebass'
 import DoubleCheckbox from './DoubleCheckbox'
+import { Flex } from 'reflexbox'
+import TutorialButton from "./TutorialButton"
 
-// On the left, a checkbox that can be clicked on twice; once to indicate proficiency, a second time to indicate expertise
-
-// Each skill has a number and a name. 20 skills or so. Listed top-to-bottom in alphabetical order.
-
-// Eventually, import number for skill from props. But for now, populate it with default numbers. No exporting needed because the calculations are made in app's state.
-
-// Lower right will have a ? that, when clicked, will open a modal explaining each skill, how the number is calculated, and what proficiency/expertise means.
+// I've kept this long list of state in to remind us what obscure stats will be needed to be kept track of, but will be deleted later.
 
 export default class SkillWidget extends Widget {
     state = {
@@ -33,209 +27,248 @@ export default class SkillWidget extends Widget {
         reProficiency: 0,
         slProficiency: 0,
         stProficiency: 0,
-        suProficiency: 0
+        suProficiency: 0,
+        tutorial: 0
     }
 
-    // tutorialOpen = () => {
+    // On the left, a checkbox that can be clicked on twice; once to indicate proficiency, a second time to indicate expertise
 
-    // }
+    // Each skill has a number and a name. 20 skills or so. Listed top-to-bottom in alphabetical order.
 
-    indicateAcProf = () => {
-        if (this.state.acProficiency === 0) { this.setState({ acProficiency: 1 }) }
+    // Eventually, import number for skill from props. But for now, populate it with default numbers. No exporting needed because the calculations are made in app's state.
 
-        else if (this.state.acProficiency === 1) { this.setState({ acProficiency: 2 }) }
-
-        else if (this.state.acProficiency === 2) { this.setState({ acProficiency: 0 }) }
-    }
-
-    indicateAhProf = () => {
-        if (this.state.ahProficiency === 0) { this.setState({ ahProficiency: 1 }) }
-
-        else if (this.state.ahProficiency === 1) { this.setState({ ahProficiency: 2 }) }
-
-        else if (this.state.ahProficiency === 2) { this.setState({ ahProficiency: 0 }) }
-    }
-
-    indicateArProf = () => {
-        if (this.state.arProficiency === 0) { this.setState({ arProficiency: 1 }) }
-
-        else if (this.state.arProficiency === 1) { this.setState({ arProficiency: 2 }) }
-
-        else if (this.state.arProficiency === 2) { this.setState({ arProficiency: 0 }) }
-    }
-
-    indicateAtProf = () => {
-        if (this.state.atProficiency === 0) { this.setState({ atProficiency: 1 }) }
-
-        else if (this.state.atProficiency === 1) { this.setState({ atProficiency: 2 }) }
-
-        else if (this.state.atProficiency === 2) { this.setState({ atProficiency: 0 }) }
-    }
-
-    indicateDeProf = () => {
-        if (this.state.deProficiency === 0) { this.setState({ deProficiency: 1 }) }
-
-        else if (this.state.deProficiency === 1) { this.setState({ deProficiency: 2 }) }
-
-        else if (this.state.deProficiency === 2) { this.setState({ deProficiency: 0 }) }
-    }
-
-    indicateHiProf = () => {
-        if (this.state.hiProficiency === 0) { this.setState({ hiProficiency: 1 }) }
-
-        else if (this.state.hiProficiency === 1) { this.setState({ hiProficiency: 2 }) }
-
-        else if (this.state.hiProficiency === 2) { this.setState({ hiProficiency: 0 }) }
-    }
-
-    indicateInsProf = () => {
-        if (this.state.insProficiency === 0) { this.setState({ insProficiency: 1 }) }
-
-        else if (this.state.insProficiency === 1) { this.setState({ insProficiency: 2 }) }
-
-        else if (this.state.insProficiency === 2) { this.setState({ insProficiency: 0 }) }
-    }
-
-    indicateIntProf = () => {
-        if (this.state.intProficiency === 0) { this.setState({ intProficiency: 1 }) }
-
-        else if (this.state.intProficiency === 1) { this.setState({ intProficiency: 2 }) }
-
-        else if (this.state.intProficiency === 2) { this.setState({ intProficiency: 0 }) }
-    }
-
-    indicateInvProf = () => {
-        if (this.state.invProficiency === 0) { this.setState({ invProficiency: 1 }) }
-
-        else if (this.state.invProficiency === 1) { this.setState({ invProficiency: 2 }) }
-
-        else if (this.state.invProficiency === 2) { this.setState({ invProficiency: 0 }) }
-    }
-
-    indicateMeProf = () => {
-        if (this.state.meProficiency === 0) { this.setState({ meProficiency: 1 }) }
-
-        else if (this.state.meProficiency === 1) { this.setState({ meProficiency: 2 }) }
-
-        else if (this.state.meProficiency === 2) { this.setState({ meProficiency: 0 }) }
-    }
-
-    indicateNaProf = () => {
-        if (this.state.naProficiency === 0) { this.setState({ naProficiency: 1 }) }
-
-        else if (this.state.naProficiency === 1) { this.setState({ naProficiency: 2 }) }
-
-        else if (this.state.naProficiency === 2) { this.setState({ naProficiency: 0 }) }
-    }
-
-    indicatePercProf = () => {
-        if (this.state.percProficiency === 0) { this.setState({ percProficiency: 1 }) }
-
-        else if (this.state.percProficiency === 1) { this.setState({ percProficiency: 2 }) }
-
-        else if (this.state.percProficiency === 2) { this.setState({ percProficiency: 0 }) }
-    }
-
-    indicatePerfProf = () => {
-        if (this.state.perfProficiency === 0) { this.setState({ perfProficiency: 1 }) }
-
-        else if (this.state.perfProficiency === 1) { this.setState({ perfProficiency: 2 }) }
-
-        else if (this.state.perfProficiency === 2) { this.setState({ perfProficiency: 0 }) }
-    }
-
-    indicatePersProf = () => {
-        if (this.state.persProficiency === 0) { this.setState({ persProficiency: 1 }) }
-
-        else if (this.state.persProficiency === 1) { this.setState({ persProficiency: 2 }) }
-
-        else if (this.state.persProficiency === 2) { this.setState({ persProficiency: 0 }) }
-    }
-
-    indicateReProf = () => {
-        if (this.state.reProficiency === 0) { this.setState({ reProficiency: 1 }) }
-
-        else if (this.state.reProficiency === 1) { this.setState({ reProficiency: 2 }) }
-
-        else if (this.state.reProficiency === 2) { this.setState({ reProficiency: 0 }) }
-    }
-
-    indicateSlProf = () => {
-        if (this.state.slProficiency === 0) { this.setState({ slProficiency: 1 }) }
-
-        else if (this.state.slProficiency === 1) { this.setState({ slProficiency: 2 }) }
-
-        else if (this.state.slProficiency === 2) { this.setState({ slProficiency: 0 }) }
-    }
-
-    indicateStProf = () => {
-        if (this.state.stProficiency === 0) { this.setState({ stProficiency: 1 }) }
-
-        else if (this.state.stProficiency === 1) { this.setState({ stProficiency: 2 }) }
-
-        else if (this.state.stProficiency === 2) { this.setState({ stProficiency: 0 }) }
-    }
-
-    indicateSuProf = () => {
-        if (this.state.suProficiency === 0) { this.setState({ suProficiency: 1 }) }
-
-        else if (this.state.suProficiency === 1) { this.setState({ suProficiency: 2 }) }
-
-        else if (this.state.suProficiency === 2) { this.setState({ suProficiency: 0 }) }
-    }
-
-    acProfCheckbox = () => {
-        if (this.state.acProficiency === 0) { return <h3 style={{ float: "left" }}>Ac not Prof</h3>; }
-        else if (this.state.acProficiency === 1) { return <h3 style={{ float: "left" }}>Ac kinda Prof</h3>; }
-        else if (this.state.acProficiency === 2) { return <h3 style={{ float: "left" }}>Ac v Prof</h3>; }
-    }
+    // Lower right will have a ? that, when clicked, will open a modal explaining each skill, how the number is calculated, and what proficiency/expertise means.
 
     render() {
         return (
             <Box>
-                <h1>Skillz</h1>
-                <p>{this.props.children}</p>
-                <DoubleCheckbox />
-                <Text
+                <Heading 
+                fontSize={[ 5, 6, 7 ]}
+                color='secondary'>
+                    {this.props.children}
+                </Heading>
+                <Flex>
+                    <DoubleCheckbox />
+                    <Text
                     fontSize={[3, 4, 5]}
                     fontWeight='bold'
-                    color='primary'>10 Acrobatics
-                Proficiency: {this.state.acProficiency}</Text>
-                <DoubleCheckbox /><p className="skill-numbers">10 Animal Handling</p>
-                <p>Proficiency: {this.state.ahProficiency}</p>
-                <DoubleCheckbox /><p className="skill-numbers">10 Arcana</p>
-                <p>Proficiency: {this.state.arProficiency}</p>
-                <DoubleCheckbox /><p className="skill-numbers">10 Athletics</p>
-                <p>Proficiency: {this.state.atProficiency}</p>
-                <DoubleCheckbox /><p className="skill-numbers">10 Deception</p>
-                <p>Proficiency: {this.state.deProficiency}</p>
-                <DoubleCheckbox /><p className="skill-numbers">10 History</p>
-                <p>Proficiency: {this.state.hiProficiency}</p>
-                <DoubleCheckbox /><p className="skill-numbers">10 Insight</p>
-                <p>Proficiency: {this.state.insProficiency}</p>
-                <DoubleCheckbox /><p className="skill-numbers">10 Intimidation</p>
-                <p>Proficiency: {this.state.intProficiency}</p>
-                <DoubleCheckbox /><p className="skill-numbers">10 Investigation</p>
-                <p>Proficiency: {this.state.invProficiency}</p>
-                <DoubleCheckbox /><p className="skill-numbers">10 Medicine</p>
-                <p>Proficiency: {this.state.meProficiency}</p>
-                <DoubleCheckbox /><p className="skill-numbers">10 Nature</p>
-                <p>Proficiency: {this.state.naProficiency}</p>
-                <DoubleCheckbox /><p className="skill-numbers">10 Perception</p>
-                <p>Proficiency: {this.state.percProficiency}</p>
-                <DoubleCheckbox /><p className="skill-numbers">10 Performance</p>
-                <p>Proficiency: {this.state.perfProficiency}</p>
-                <DoubleCheckbox /><p className="skill-numbers">10 Persuasion</p>
-                <p>Proficiency: {this.state.persProficiency}</p>
-                <DoubleCheckbox /><p className="skill-numbers">10 Religion</p>
-                <p>Proficiency: {this.state.reProficiency}</p>
-                <DoubleCheckbox /><p className="skill-numbers">10 Sleight of Hand</p>
-                <p>Proficiency: {this.state.slProficiency}</p>
-                <DoubleCheckbox /><p className="skill-numbers">10 Stealth</p>
-                <p>Proficiency: {this.state.stProficiency}</p>
-                <DoubleCheckbox /><p className="skill-numbers">10 Survival</p>
-                <p>Proficiency: {this.state.suProficiency}</p>
-                <button className="tutorial-button" /* onClick={this.tutorialOpen}*/>Tutorial Button</button>
+                    color='primary'>
+                    &#8287;
+                    10 
+                    &#8287;
+                    Acrobatics</Text>
+                </Flex>
+                <Flex>
+                    <DoubleCheckbox />
+                    <Text
+                    fontSize={[3, 4, 5]}
+                    fontWeight='bold'
+                    color='primary'>
+                    &#8287;
+                    10
+                    </Text> 
+                    <Text
+                    fontSize={[2, 3, 4]}
+                    fontWeight='bold'
+                    color='primary'>
+                    &#8287;
+                    Animal Handling
+                    </Text>
+                </Flex>
+                <Flex>
+                    <DoubleCheckbox />
+                    <Text
+                    fontSize={[3, 4, 5]}
+                    fontWeight='bold'
+                    color='primary'>
+                    &#8287;
+                    10 
+                    &#8287;
+                    Arcana
+                    </Text>
+                </Flex>
+                <Flex>
+                    <DoubleCheckbox />
+                    <Text
+                    fontSize={[3, 4, 5]}
+                    fontWeight='bold'
+                    color='primary'>
+                    &#8287;
+                    10 
+                    &#8287;
+                    Athletics
+                    </Text>
+                </Flex>
+                <Flex>
+                    <DoubleCheckbox />
+                    <Text
+                    fontSize={[3, 4, 5]}
+                    fontWeight='bold'
+                    color='primary'>
+                    &#8287;
+                    10 
+                    &#8287;
+                    Deception
+                    </Text>
+                </Flex>
+                <Flex>
+                    <DoubleCheckbox />
+                    <Text
+                    fontSize={[3, 4, 5]}
+                    fontWeight='bold'
+                    color='primary'>
+                    &#8287;
+                    10 
+                    &#8287;
+                    History
+                    </Text>
+                </Flex>
+                <Flex>
+                    <DoubleCheckbox />
+                    <Text
+                    fontSize={[3, 4, 5]}
+                    fontWeight='bold'
+                    color='primary'>
+                    &#8287;
+                    10 
+                    &#8287;
+                    Insight
+                    </Text>
+                </Flex>
+                <Flex>
+                    <DoubleCheckbox />
+                    <Text
+                    fontSize={[3, 4, 5]}
+                    fontWeight='bold'
+                    color='primary'>
+                    &#8287;
+                    10 
+                    &#8287;
+                    Intimidation
+                    </Text>
+                </Flex>
+                <Flex>
+                    <DoubleCheckbox />
+                    <Text
+                    fontSize={[3, 4, 5]}
+                    fontWeight='bold'
+                    color='primary'>
+                    &#8287;
+                    10 
+                    &#8287;
+                    Investigation
+                    </Text>
+                </Flex>
+                <Flex>
+                    <DoubleCheckbox />
+                    <Text
+                    fontSize={[3, 4, 5]}
+                    fontWeight='bold'
+                    color='primary'>
+                    &#8287;    
+                    10 
+                    &#8287;
+                    Medicine
+                    </Text>
+                </Flex>
+                <Flex>
+                    <DoubleCheckbox />
+                    <Text
+                    fontSize={[3, 4, 5]}
+                    fontWeight='bold'
+                    color='primary'>
+                    &#8287;
+                    10 
+                    &#8287;
+                    Nature
+                    </Text>
+                </Flex>
+                <Flex>
+                    <DoubleCheckbox />
+                    <Text
+                    fontSize={[3, 4, 5]}
+                    fontWeight='bold'
+                    color='primary'>
+                    &#8287;
+                    10 
+                    &#8287;
+                    Perception
+                    </Text>
+                </Flex>
+                <Flex>
+                    <DoubleCheckbox />
+                    <Text
+                    fontSize={[3, 4, 5]}
+                    fontWeight='bold'
+                    color='primary'>
+                    &#8287;
+                    10 
+                    &#8287;
+                    Performance
+                    </Text>
+                </Flex>
+                <Flex>
+                    <DoubleCheckbox />
+                    <Text
+                    fontSize={[3, 4, 5]}
+                    fontWeight='bold'
+                    color='primary'>
+                    &#8287;
+                    10 
+                    &#8287;
+                    Persuasion
+                    </Text>
+                </Flex>
+                <Flex>
+                    <DoubleCheckbox />
+                    <Text
+                    fontSize={[3, 4, 5]}
+                    fontWeight='bold'
+                    color='primary'>
+                    &#8287;
+                    10 
+                    &#8287;
+                    Religion
+                    </Text>
+                </Flex>
+                <Flex>
+                    <DoubleCheckbox />
+                    <Text
+                    fontSize={[3, 4, 5]}
+                    fontWeight='bold'
+                    color='primary'>
+                    &#8287;
+                    10
+                    Sleight of Hand
+                    </Text>
+                </Flex>
+                <Flex>
+                    <DoubleCheckbox />
+                    <Text
+                    fontSize={[3, 4, 5]}
+                    fontWeight='bold'
+                    color='primary'>
+                    &#8287;
+                    10 
+                    &#8287;
+                    Stealth
+                    </Text>
+                </Flex>
+                <Flex>
+                    <DoubleCheckbox />
+                    <Text
+                    fontSize={[3, 4, 5]}
+                    fontWeight='bold'
+                    color='primary'>
+                    &#8287;
+                    10 
+                    &#8287;
+                    Survival
+                    </Text>
+                </Flex>
+                <Flex>
+                    <TutorialButton tutorial="skill"/>
+                </Flex>
             </Box>
         )
     }
