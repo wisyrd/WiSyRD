cac9b4e4e5fd126338f7fa9330158df4ca40ecb2
Magnus Appel <68828420+magnuss86@users.noreply.github.com>
2020-11-17 11:32:33 -0800

    added row add button for more attacks

---
+++ src/components/AttackRow.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Box, Flex, Button, Heading, Text } from 'rebass';
+import { Input } from "@rebass/forms";
+
+export default function AttackRow() {
+    return (
+        <Flex>
+            {/* <Label htmlFor="Hit">Hit</Label> */}
+            <Input
+              name="hit"
+              type="text"
+              placeholder="Hit Mod"
+              width={1 / 3}
+            />
+            {/* <Label htmlFor="Range">Range</Label> */}
+            <Input
+              name="range"
+              type="text"
+              placeholder="Attack Range "
+              width={1 / 3}
+            />
+            {/* <Label htmlFor="DamgageType">Damage/Type</Label> */}
+            <Input
+              name="damageType"
+              type="text"
+              placeholder="Damage"
+              width={1 / 3}
+            />
+          </Flex>
+    )
+}
