72707c697897d9fd7276b949f9d89c6c4a2645da
Quint Turner <henryquintturner@gmail.com>
2020-11-18 14:55:12 -0800

    u thought this was bad?? it's good

--- src/components/RitualButton.js
+++ src/components/RitualButton.js
@@ -1,20 +1,12 @@
 import React, { useState } from "react";
 import { Box } from 'rebass'
-import uncheckedcheckbox from '../images/unchecked-checkbox.png';
 import ritualcheckbox from '../images/ritual-checkbox.jpg';
 
 export default function RitualButton() {
-    const [ritualState, setRitualState] = useState(uncheckedcheckbox);
-
-    function indicateState(event) {
-        if (ritualState == uncheckedcheckbox) { setRitualState(ritualcheckbox) }
-
-        else if (ritualState == ritualcheckbox) { setRitualState(uncheckedcheckbox) }
-    }
-
+    const [ritualState, setRitualState] = useState(ritualcheckbox);
         return (
             <Box className="single-checkbox">
-                <img src={ritualState} alt="unchecked-checkbox" width="50" height="50" onClick={indicateState} />
+                <img src={ritualState} alt="unchecked-checkbox" width="50" height="50" />
             </Box>
         )
 }
