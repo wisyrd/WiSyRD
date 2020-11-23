47c2cd9f646392288acf916a58c32426c451dcb3
Jacob Cowan <mrjcowman@gmail.com>
2020-11-23 05:23:01 -0800

    Set up to use global state saving

--- src/components/widgets/NotesWidget.js
+++ src/components/widgets/NotesWidget.js
@@ -11,13 +11,20 @@ export default class NotesWidget extends Widget {
         this.title = "Notes Widget";
         this.widgetType = "notes-widget"
         this.tutorialText = <Text>This is a basic widget for storing any information needed in text format. It may be editted at will and is there for refernce.</Text>
+    
+        this.initializeIfNew();
+    }
+
+    initialize=()=>{
+        this.setExportedValue("text", "")
     }
 
     renderPanel = () => {
+        let text = this.props.widgetState.exports.text || "";
         return (<>
             <Box variant='backgroundBox'>
                 <Box variant='fullWidthBox'>
-                    <Textarea variant='textArea' placeholder='Add notes' onChange={this.handleChange} style={{resize:"none"}}></Textarea>
+                    <Textarea variant='textArea' placeholder='Add notes' onChange={this.handleChange} style={{resize:"none"}} defaultValue={text}></Textarea>
                 </Box>
             </Box>
         </>
